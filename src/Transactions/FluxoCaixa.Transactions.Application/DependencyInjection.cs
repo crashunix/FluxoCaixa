dbf0ca74cad785413d35234c60115cbc96a4e5fc
Juan Gomes Macario <jgomesmacario@outlook.com>
2026-08-21 23:23:33 -0300

    integrar corretamente validators

--- src/Transactions/FluxoCaixa.Transactions.Application/DependencyInjection.cs
+++ src/Transactions/FluxoCaixa.Transactions.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using FluxoCaixa.Transactions.Application.Common.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddTransactionsApplication(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         services.AddMediatR(cfg => {
             cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
